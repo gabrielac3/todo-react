2c0153d967213282089b97aa24eb29f30692cad4
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-10-30 16:01:32 -0500

    versión 1

---
+++ src/CreateTodoButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './CreateTodoButton.css';
+
+function CreateTodoButton(props) {
+  const onClickButton = (msg) => {
+    alert(msg);
+  };
+  
+  return (
+    <>
+        {/* ✅ */}
+        <button
+          className="CreateTodoButton"
+          onClick={() => onClickButton('Aquí se debería abrir el modal')}
+        >
+          +
+        </button>
+
+    </>
+  );
+}
+
+export { CreateTodoButton };
